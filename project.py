ba9aae50b5e6c945284cf6d14adf48debe8da16b
Michael Ryden <m_ryden@yahoo.com>
2017-09-04 12:18:47 -0500

    adding client_id to project.py

--- project.py
+++ project.py
@@ -16,6 +16,10 @@ import requests
 
 app = Flask(__name__)
 
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Restaurant Menu Application"
+
 # Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
